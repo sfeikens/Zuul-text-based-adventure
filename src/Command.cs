f93a0cd3029be74e48baf19edfaf62e77e630b26
sfeikens <s.feikens@student.alfa-college.nl>
2026-01-25 13:03:03 +0100

    Add third word functionality

--- src/Command.cs
+++ src/Command.cs
@@ -2,13 +2,15 @@ class Command
 {
 	public string CommandWord { get; init; }
 	public string SecondWord { get; init; }
+	public string ThirdWord {get; init; }
 	
 	// Create a command object. First and second word must be supplied, but
 	// either one (or both) can be null. See Parser.GetCommand()
-	public Command(string first, string second)
+	public Command(string first, string second, string third)
 	{
 		CommandWord = first;
 		SecondWord = second;
+		ThirdWord = third;
 	}
 
 	
@@ -23,5 +25,11 @@ class Command
 	public bool HasSecondWord()
 	{
 		return SecondWord != null;
+	}
+
+	// Return true if the command has a third word
+	public bool HasThirdWord()
+	{
+		return ThirdWord != null;
 	}
 }
